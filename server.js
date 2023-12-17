ebf23f02d550e177a397c7c35a3e263b0e7a7917
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-12-17 17:43:06 +0530

    simple server serving array of objects json

--- server.js
+++ server.js
@@ -2,15 +2,36 @@
 
 const http = require('http');
 
+const notes = [
+    {
+        id: 1,
+        content: 'backend using node.js',
+        important: true
+    },
+    {
+        id: 2,
+        content: 'node.js is a open source',
+        important: false
+    },
+    {
+        id: 3,
+        content: 'simple web server using node.js',
+        important: true
+    },
+]
+
+
+
 //create a simple web server
 
 const app = http.createServer((request, response) =>{
-    response.writeHead(200, { 'content-Type' : 'text/plain'});
-    response.end('Hello World!');
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(notes));
 
 });
 
 const PORT = 3001;
 //make the server to listen the http requests
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+

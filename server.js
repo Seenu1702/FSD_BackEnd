716edc85386e7794bb90d62e92ddf1f67fa209bb
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-12-18 17:46:30 +0530

    endpoint to fetch a single note

--- server.js
+++ server.js
@@ -31,6 +31,21 @@ app.get('/api/notes', (request, response) => {
     response.json(notes);
 });
 
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+
+    // find the notes with the id
+    const note = notes.find(note => note.id == id);
+    console.log(note);
+
+    if(note){
+        response.status(200).json(note);
+    }
+    else{
+        response.status(404).json({message: 'id does not exist'});
+    }
+});
+
 
 const Hostname = '127.0.0.1';
 const PORT = 3001;

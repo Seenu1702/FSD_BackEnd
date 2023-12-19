4686f6ff7e50a46988eb069d71cbe1d5f6ac14d7
Seenu1702 <seenusubramaniyan17@gmail.com>
2023-12-19 16:34:06 +0530

    moved app code to index.js

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const config = require('./utils/config');
 const {info, error} = require('./utils/logger');
 
 //make the server to listen the http requests
-app.listen(config.PORT, () => info(`Server running at htpp://${config.HOSTNAME}:${config.PORT}`));
+app.listen(config.PORT, () => {
+    info(`Server running at htpp://${config.HOSTNAME}:${config.PORT}`)
+});
